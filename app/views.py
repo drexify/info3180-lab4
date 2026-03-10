aa9ad33cec6770a681a09c5317fdeaa271f75c7d
Dre <randrefearon@gmail.com>
2026-03-10 11:54:21 -0500

    Completed Exercise 5

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from flask import render_template, request, redirect, url_for, flash, session, a
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
-from app.forms import LoginForm
+from app.forms import LoginForm, UploadForm
 from werkzeug.security import check_password_hash
 
 
@@ -25,17 +25,20 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
-    # Instantiate your form class
+    form = UploadForm()
 
     # Validate file upload on submit
     if form.validate_on_submit():
         # Get file data and save to your uploads folder
-
+        image = form.image.data
+        filename = secure_filename(image.filename)
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         flash('File Saved', 'success')
         return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
 
-    return render_template('upload.html')
+    return render_template('upload.html', form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -43,18 +46,18 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-		username = form.username.data
-		password = form.password.data
-		
-		user = UserProfile.query.filter_by(username=username).first()
-		
-		# Note: if user is the same as saying if user is not None
-		if user and check_password_hash(user.password, password):
-			login_user(user)
-			flash("Login Successful")
-			return redirect(url_for("upload"))
-		else:
-			flash("Login Unsuccessful")
+        username = form.username.data
+        password = form.password.data
+        
+        user = UserProfile.query.filter_by(username=username).first()
+        
+        # Note: if user is the same as saying if user is not None
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash("Login Successful")
+            return redirect(url_for("upload"))
+        else:
+            flash("Login Unsuccessful")
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
